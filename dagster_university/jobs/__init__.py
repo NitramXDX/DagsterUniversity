c66b488feddb2c45c928532b2bc0dbf7de1c622b
MartinLB <martingedo@gmail.com>
2024-06-07 13:07:06 +0200

    Lesson 8 completed

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -1,13 +1,14 @@
 from dagster import AssetSelection, define_asset_job
-
-trips_by_week = AssetSelection.assets("trips_by_week")
+from ..partitions import monthly_partition, weekly_partition
 
 trip_update_job = define_asset_job(
     name="trip_update_job",
-    selection=AssetSelection.all() - trips_by_week
+    partitions_def=monthly_partition, # partitions added here
+    selection=AssetSelection.all() - AssetSelection.assets("trips_by_week")
 )
 
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
-    selection=trips_by_week,
+    partitions_def=weekly_partition,
+    selection=AssetSelection.assets("trips_by_week"),
 )
